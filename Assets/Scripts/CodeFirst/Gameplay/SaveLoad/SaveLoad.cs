5d9ba23b00c374e04af7057f616ecadad8465ba6
Pavel Vinogradov <pavel.vinogradov@litslink.com>
2024-05-02 01:22:07 +0300

    - update saveload to outstand file path url;

--- Assets/Scripts/CodeFirst/Gameplay/SaveLoad/SaveLoad.cs
+++ Assets/Scripts/CodeFirst/Gameplay/SaveLoad/SaveLoad.cs
@@ -1,20 +1,24 @@
+using System.IO;
 using UnityEngine;
 
 namespace CodeFirst.Gameplay
 {
     public static class SaveLoad
 	{
+		public static string fileToSavePath = Application.streamingAssetsPath + "/LastBoardData.json";
+
 		public static Board Load(Board board)
 		{
-			var result = System.IO.File.ReadAllText(Application.streamingAssetsPath + "/LastBoardData.json");
 			board = null;
+			var result = File.ReadAllText(fileToSavePath);
 			return JsonUtility.FromJson<Board>(result);
 		}
 
 		public static void Save(Board board)
 		{
+			File.Delete(fileToSavePath);
 			string dataToSave = JsonUtility.ToJson(board);
-			System.IO.File.WriteAllText(Application.streamingAssetsPath + "/LastBoardData.json", dataToSave);
+			File.WriteAllText(fileToSavePath, dataToSave);
 		}
 	}
 }
